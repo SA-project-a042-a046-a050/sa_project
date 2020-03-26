2f6a407257612f22d56a10b7cd26c6d1ad9553f1
SA-project-a042-a046-a050 <62700849+SA-project-a042-a046-a050@users.noreply.github.com>
2020-03-26 17:20:58 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 12:44:16 2020
+
+@author: utkar
+"""
+
+print("This is a python files saved on the github repository")
+print("this file is edited on the local machine")
